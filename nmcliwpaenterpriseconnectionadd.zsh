fb0ee270e24fbaefe35ce6e942bdcc109051e444
AmberGraph41161 <AmberGraph41161@gmail.com>
2026-02-18 19:48:21 -0600

    add enterprise wifi connection add helper script

--- nmcliwpaenterpriseconnectionadd.zsh
+++ nmcliwpaenterpriseconnectionadd.zsh
@@ -1,6 +1,11 @@
 #!/bin/zsh
 
-connection_name="$(nmcli dev wifi | sed '1,1d; s/........//' | awk '{ print $8, $1, $2 }' | fuzzel --dmenu -p'connection name > ')"
+notificationlifetime=5000
+notificationid=100000000
+
+connection_name="$(nmcli -t -g 'bars, bssid, ssid' -e no -m multiline dev wifi | paste - - - \
+	| fuzzel --dmenu -p'connection name > ' \
+	| sed 's/.*\tSSID://')"
 if [ -z "$connection_name" ]; then
 	exit
 fi
@@ -15,4 +20,8 @@ if [ -z "$password" ]; then
 	exit
 fi
 
-nmcli connection add type wifi con-name $connection_name ifname wlp1s0 ssid $connection_name -- wifi-sec.key-mgmt wpa-eap 802-1x.eap peap 802-1x.phase2-auth mschapv2 802-1x.identity $username 802-1x.password $password
+if ! nmcli connection add type wifi con-name $connection_name ifname wlp1s0 ssid $connection_name -- wifi-sec.key-mgmt wpa-eap 802-1x.eap peap 802-1x.phase2-auth mschapv2 802-1x.identity $username 802-1x.password $password; then
+	notify-send "FAILED TO ADD CONNECT TO:" "$connection_name" -u normal -t $notificationlifetime -r $notificationid
+else
+	notify-send "SUCCESSFULLY CONNECTED TO:" "$connection_name" -u normal -t $notificationlifetime -r $notificationid
+fi
