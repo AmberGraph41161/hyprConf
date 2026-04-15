7a43f218982b47c1e9713be1ce0b51fef5cbe5ae
AmberGraph41161 <AmberGraph41161@gmail.com>
2026-04-15 16:32:52 -0500

    clear middle click copy buffer (primary clipboard) on cliphistdelete

--- cliphistdelete.zsh
+++ cliphistdelete.zsh
@@ -3,6 +3,8 @@
 uniqueString='DELETE_ALL_'"$(date '+%F.%H.%M.%S')"
 selected="$({ echo $uniqueString; cliphist list } | fuzzel --dmenu --prompt='cliphist DELETE entry > ')"
 
+wl-copy --primary --clear
+
 if [ "$selected" = "$uniqueString" ]; then
 	cliphist wipe
 else
