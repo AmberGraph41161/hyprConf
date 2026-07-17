a619496933a23daacf047c3f574f13133e2253d3
AmberGraph41161 <AmberGraph41161@gmail.com>
2026-07-17 15:55:07 -0400

    fix battery status; no need for double double-quotes

--- scripts/batterystatusnotificationsend.zsh
+++ scripts/batterystatusnotificationsend.zsh
@@ -7,15 +7,15 @@ MaxCapacity=$(echo MaxCapacity: $(cat /sys/class/power_supply/BAT0/capacity 2> /
 CapacityLevel=$(echo CapacityLevel: $(cat /sys/class/power_supply/BAT0/capacity_level 2> /dev/null))
 Cycles=$(echo Cycles: $(cat /sys/class/power_supply/BAT0/cycle_count 2> /dev/null))
 AC=$(echo AC: $(cat /sys/class/power_supply/AC/online 2> /dev/null))
-Status=$(echo Status: $(/sys/class/power_supply/BAT0/status 2> /dev/null))
+Status=$(echo Status: $(cat /sys/class/power_supply/BAT0/status 2> /dev/null))
 ChargeStartThreshold=$(echo ChargeStartThreshold: $(cat /sys/class/power_supply/BAT0/charge_start_threshold 2> /dev/null))
 
-batteryinfomation="$acpiInformation""\n"
-batteryinfomation+="$MaxCapacity""\n"
-batteryinfomation+="$CapacityLevel""\n"
-batteryinfomation+="$Cycles""\n"
-batteryinfomation+="$AC""\n"
-batteryinfomation+="$Status""\n"
+batteryinfomation="$acpiInformation\n"
+batteryinfomation+="$MaxCapacity\n"
+batteryinfomation+="$CapacityLevel\n"
+batteryinfomation+="$Cycles\n"
+batteryinfomation+="$AC\n"
+batteryinfomation+="$Status\n"
 batteryinfomation+="$ChargeStartThreshold"
 
 notify-send "Battery Info" "$batteryinfomation" -t $notificationlifetime -r $notificationid -h "int:value:$(cat /sys/class/power_supply/BAT0/capacity)"
