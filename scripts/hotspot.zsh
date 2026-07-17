00dab0d342b5b6fe572954da8f83a674632d1e61
AmberGraph41161 <AmberGraph41161@gmail.com>
2026-07-17 15:51:16 -0400

    early exit on blank hotspot fields/prompts

--- scripts/hotspot.zsh
+++ scripts/hotspot.zsh
@@ -1,14 +1,23 @@
 #!/bin/zsh
 
 ifname="$(nmcli -t dev | fuzzel --dmenu -p'ifname > ' | awk -F':' '{ print $1 }')"
-ssid=$HOST
-conName=$HOST
+[ -z "$ifname" ] && exit
 
-if [ "$(echo "hostname\ncustom" | fuzzel --dmenu -p'ssid/con-name > ')" = "custom" ]; then
-	ssid="$(fuzzel --dmenu --prompt-only='ssid > ')"
-	conName="$(fuzzel --dmenu --prompt-only='con-name > ')"
-fi
+case "$(echo -e 'hostname\ncustom' | fuzzel --dmenu -p'ssid/con-name > ')" in
+
+	"custom")
+		ssid="$(fuzzel --dmenu --prompt-only='ssid > ')"
+		conName="$(fuzzel --dmenu --prompt-only='con-name > ')"
+		;;
+	"hostname")
+		ssid=$HOST
+		conName=$HOST
+		;;
+	*)
+		exit
+esac
 
 password="$(fuzzel --dmenu --password --prompt-only='password > ')"
+[ -z "$password" ] && exit
 
 nmcli dev wifi hotspot ifname "$ifname" ssid "$ssid" con-name "$conName"
