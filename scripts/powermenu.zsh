bbaa3ada8cf26684a51a8886216cf42300ba3ec2
AmberGraph41161 <AmberGraph41161@gmail.com>
2026-08-03 20:08:24 -0500

    hyprshutdown before reboot

--- scripts/powermenu.zsh
+++ scripts/powermenu.zsh
@@ -30,7 +30,7 @@ case $(echo $menuoptions | fuzzel -p "[PowerMenu] > " --dmenu) in
 
 		areyousure=$(echo $areyousure | fuzzel -p'[ARE YOU SURE??] ' --dmenu);
 		if [ "$areyousure" = "I am absolutely sure I want to reboot" ]; then
-			reboot
+			hyprshutdown -p 'reboot'
 		fi
 		;;
 
