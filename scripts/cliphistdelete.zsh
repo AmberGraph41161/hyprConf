d1f64a1b9c3123323ff8bfe3c69ab6aead4b29ee
AmberGraph41161 <AmberGraph41161@gmail.com>
2026-08-26 00:22:06 -0500

    wipe clipboard and primary clipboard when cliphist delete
    idk how this didnt bother me until now, but I guess its finally fixed
    now lol

--- scripts/cliphistdelete.zsh
+++ scripts/cliphistdelete.zsh
@@ -3,10 +3,12 @@
 uniqueString='DELETE_ALL_'"$(date '+%F.%H.%M.%S')"
 selected="$({ echo $uniqueString; cliphist list } | fuzzel --dmenu --prompt='cliphist DELETE entry > ')"
 
-wl-copy --primary --clear
-
 if [ "$selected" = "$uniqueString" ]; then
 	cliphist wipe
-else
+	wl-copy --primary --clear
+	wl-copy --clear
+elif [ -n "$selected" ]; then
 	echo "$selected" | cliphist delete
+	wl-copy --primary --clear
+	wl-copy --clear
 fi
