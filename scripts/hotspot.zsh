ff43b4dbf9a58cb02fcb8f2b9f70f16e07a91f38
AmberGraph41161 <AmberGraph41161@gmail.com>
2026-08-25 23:10:28 -0500

    show hotspot.zsh fail/success notification

--- scripts/hotspot.zsh
+++ scripts/hotspot.zsh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+notificationid=100000000
+notificationlifetime=10000
+
 ifname="$(nmcli -t dev | fuzzel --dmenu -p'ifname > ' | awk -F':' '{ print $1 }')"
 [ -z "$ifname" ] && exit
 
@@ -7,7 +10,9 @@ case "$(echo -e 'hostname\ncustom' | fuzzel --dmenu -p'ssid/con-name > ')" in
 
 	"custom")
 		ssid="$(fuzzel --dmenu --prompt-only='ssid > ')"
+		[ -z "$ssid" ] && exit
 		conName="$(fuzzel --dmenu --prompt-only='con-name > ')"
+		[ -z "$conName" ] && exit
 		;;
 	"hostname")
 		ssid=$HOST
@@ -20,4 +25,7 @@ esac
 password="$(fuzzel --dmenu --password --prompt-only='password > ')"
 [ -z "$password" ] && exit
 
-nmcli dev wifi hotspot ifname "$ifname" ssid "$ssid" con-name "$conName"
+output=$(nmcli dev wifi hotspot ifname "$ifname" ssid "$ssid" con-name "$conName" 2>&1)
+((? == 0)) \
+	&& notify-send 'HOTSPOT UP' "$output" -u normal -t $notificationlifetime -r $notificationid \
+	|| notify-send 'HOTSPOT FAILED' "$output" -u critical -t $notificationlifetime -r $notificationid
